dc74fd254bec11f57ffce3eb82d9ed7c66d9a5fe
Trang <Trang@DESKTOP-A5MEN17>
2024-08-24 10:41:24 +0700

    Add project files.

---
+++ src/Acme.BookStore.Blazor.Client/Navigation/BookStoreMenuContributor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Acme.BookStore.Localization;
+using Acme.BookStore.Permissions;
+using Acme.BookStore.MultiTenancy;
+using Volo.Abp.Account.Localization;
+using Volo.Abp.UI.Navigation;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.SettingManagement.Blazor.Menus;
+using Volo.Abp.Users;
+using Volo.Abp.TenantManagement.Blazor.Navigation;
+using Volo.Abp.Identity.Blazor;
+
+namespace Acme.BookStore.Blazor.Client.Navigation;
+
+public class BookStoreMenuContributor : IMenuContributor
+{
+    private readonly IConfiguration _configuration;
+
+    public BookStoreMenuContributor(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task ConfigureMenuAsync(MenuConfigurationContext context)
+    {
+        if (context.Menu.Name == StandardMenus.Main)
+        {
+            await ConfigureMainMenuAsync(context);
+        }
+        else if (context.Menu.Name == StandardMenus.User)
+        {
+            await ConfigureUserMenuAsync(context);
+        }
+    }
+
+    private async Task ConfigureMainMenuAsync(MenuConfigurationContext context)
+    {
+        var l = context.GetLocalizer<BookStoreResource>();
+
+        context.Menu.Items.Insert(
+            0,
+            new ApplicationMenuItem(
+                "BookStore.Home",
+                l["Menu:Home"],
+                "/",
+                icon: "fas fa-home"
+            )
+        );
+
+        var bookStoreMenu = new ApplicationMenuItem(
+            "BooksStore",
+            l["Menu:BookStore"],
+            icon: "fa fa-book"
+        );
+
+        context.Menu.AddItem(bookStoreMenu);
+
+        //CHECK the PERMISSION
+        if (await context.IsGrantedAsync(BookStorePermissions.Books.Default))
+        {
+            bookStoreMenu.AddItem(new ApplicationMenuItem(
+                "BooksStore.Books",
+                l["Menu:Books"],
+                url: "/books"
+            ));
+        }
+
+        if (await context.IsGrantedAsync(BookStorePermissions.Authors.Default))
+        {
+            context.Menu.AddItem(new ApplicationMenuItem(
+                "BooksStore.Authors",
+                l["Menu:Authors"],
+                url: "/authors"
+            ));
+        }
+
+    }
+
+
+    private async Task ConfigureUserMenuAsync(MenuConfigurationContext context)
+    {
+        var accountStringLocalizer = context.GetLocalizer<AccountResource>();
+        var authServerUrl = _configuration["AuthServer:Authority"] ?? "";
+
+        context.Menu.AddItem(new ApplicationMenuItem(
+            "Account.Manage",
+            accountStringLocalizer["MyAccount"],
+            $"{authServerUrl.EnsureEndsWith('/')}Account/Manage?returnUrl={_configuration["App:SelfUrl"]}",
+            icon: "fa fa-cog",
+            order: 1000,
+            null).RequireAuthenticated());
+
+        await Task.CompletedTask;
+    }
+}
